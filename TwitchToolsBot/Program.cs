d3016770ef7138c252af18af240b1008fe191cd9
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-05-29 16:54:55 +0300

    Change syntax

--- TwitchToolsBot/Program.cs
+++ TwitchToolsBot/Program.cs
@@ -1,91 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Timers;
 
 namespace TwitchToolsBot
 {
     class Program
     {
-        private static Boolean cleaner = false;
+        private static Boolean _cleaner = false;
         static void Main(string[] args)
         {
-            Timer aTimer  = new System.Timers.Timer();
-            aTimer.Interval = 10000;
-            aTimer.Enabled = true;
+            TrendBot();
+        }
+        
+        #region trendBotRegion
+        public static void TrendBot()
+        {
             IrcClient client = new IrcClient("irc.twitch.tv", 6667, "chattrack", Secrets.OAuthSecret, Secrets.trackChannel);
             Dictionary<string, int> trendDictionary = new Dictionary<string, int>();
             var pinger = new Pinger(client);
+            Timer ıntervalTimer = new System.Timers.Timer {Interval = 10000, Enabled = true};
             pinger.Start();
-            aTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            ıntervalTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
             while (true)
             {
                 Console.WriteLine("Reading message");
                 var message = client.ReadMessage();
                 if(message != null)
                 {
-
-                    string output2 = messageOutputFiner(message);
+                    var output2 = MessageOutputFiner(message);
                     Console.WriteLine($"Message: {output2}");
-                    trendDictionary = addMessageWordsToList(output2, trendDictionary);
+                    trendDictionary = AddMessageWordsToList(output2, trendDictionary);
                     foreach (KeyValuePair<string, int> kvp in trendDictionary)
                     {
                         Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
                     }
-                    string trend = findTrend(trendDictionary, 3);
-                    if (cleaner.Equals(true))
+                    var trend = FindTrend(trendDictionary, 3);
+                    if (_cleaner.Equals(true))
                     {
                         trendDictionary.Clear();
-                        cleaner = false;
+                        _cleaner = false;
                     }
-                    if (trend != "") { Console.WriteLine("We have a trend !!" + trend); client.SendChatMessage("New Trend! : '" + trend.ToUpper() + "' @" + Secrets.trackChannel); }
+
+                    if (trend == "") continue;
+                    Console.WriteLine("We have a trend !!" + trend); client.SendChatMessage("New Trend! : '" + trend.ToUpper() + "' @" + Secrets.trackChannel);
                 }
             }
         }
 
-        private static string messageOutputFiner(string message)
+        private static string MessageOutputFiner(string message)
         {
             var output = message.Substring(message.IndexOf(':') + 1);
             var output2 = output.Substring(output.IndexOf(':') + 1);
             return output2;
         }
 
-        private static string findTrend(Dictionary<string, int> inDictionary,int trendLimit)
+        private static string FindTrend(Dictionary<string, int> inDictionary,int trendLimit)
         {
-            int mostTrendNum = 0;
-            string mostTrendStr = "1";
-            foreach (KeyValuePair<string, int> kvp in inDictionary)
+            var mostTrendNum = 0;
+            var mostTrendStr = "1";
+            foreach (var kvp in inDictionary.Where(kvp => mostTrendNum < kvp.Value))
             {
-
-                if(mostTrendNum < kvp.Value) { mostTrendNum = kvp.Value; mostTrendStr = kvp.Key; }
-
+                mostTrendNum = kvp.Value; mostTrendStr = kvp.Key;
             }
-            if(mostTrendNum > trendLimit)
-            {
-                return mostTrendStr;
-            }
-
-            return "";
+            return mostTrendNum > trendLimit ? mostTrendStr : "";
         }
 
         private static void DisplayTimeEvent(object source, ElapsedEventArgs e)
         {
-            cleaner = true;
+            _cleaner = true;
         }
 
-        private static Dictionary<string, int> addMessageWordsToList(string input,Dictionary<string, int> inDictionary)
+        private static Dictionary<string, int> AddMessageWordsToList(string input,Dictionary<string, int> inDictionary)
         {
             Dictionary<string, int> trendDictionary = inDictionary;
-            string[] inputWords = input.Split(' ');
-            for(int i = 0; i < inputWords.Length; i++)
+            var inputWords = input.Split(' ');
+            foreach (var word in inputWords)
             {
-                if (!trendDictionary.ContainsKey(inputWords[i]))
+                if (!trendDictionary.ContainsKey(word))
                 {
-                    trendDictionary.Add(inputWords[i], 1);
+                    trendDictionary.Add(word, 1);
                 }
-                else if(trendDictionary.ContainsKey(inputWords[i]))
+                else if(trendDictionary.ContainsKey(word))
                 {
-                    int value = trendDictionary[inputWords[i]];
-                    trendDictionary[inputWords[i]] = value + 1;
+                    int value = trendDictionary[word];
+                    trendDictionary[word] = value + 1;
                 }
             }
             return trendDictionary;
@@ -96,4 +95,12 @@ namespace TwitchToolsBot
         
         
     }
+    #endregion
+
+    #region desicionMakerRegion
+
+    
+
+    #endregion
 }
+
